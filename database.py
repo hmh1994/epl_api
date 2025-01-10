244310fdffa8c83ef901e1ec39dea00aaf71a04f
Ubuntu <ubuntu@ip-172-31-46-50.ap-northeast-2.compute.internal>
2025-01-10 08:51:12 +0000

    Basie code of epl_api

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-#DATABASE_URL = " "
-
-engine = create_engine(DATABASE_URL)
+DB_URL = "postgresql://postgres:holders-hyah-movin-latch@football-data-v1.cb0kwomosfy3.ap-northeast-2.rds.amazonaws.com/postgres"
+engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
