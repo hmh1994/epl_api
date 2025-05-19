9f2b9106934ec54a4d47fd78900bd49a7fec1da8
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-05-19 14:57:36 +0900

    upcoming_matches

--- games_api.py
+++ games_api.py
@@ -26,20 +26,26 @@ def overall_teamrank(db : Session = Depends(get_db)):
 @router.get("/upcomingGames")
 def upcomingGames(db: Session = Depends(get_db)):
     query = text("""
-                    SELECT 
-            f.id AS fixture_id,
-            th.short_name_en AS home_team,
-            ta.short_name_en AS away_team,
-            f.kickoff_time,
-            f.home_team_score,
-            f.away_team_score
-            FROM fixtures f
-            JOIN teams th ON f.home_team_id = th.id
-            JOIN teams ta ON f.away_team_id = ta.id
-            WHERE f.season_id = 'PULSELIVE_SEASON_719'
-            AND f.kickoff_time > NOW()
-            ORDER BY f.kickoff_time ASC
-            LIMIT 5
+                   SELECT 
+    ROW_NUMBER() OVER (ORDER BY f.kickoff_time ASC) AS no,
+    f.id,
+    ht.name_en AS home_team,
+    at.name_en AS away_team,
+    f.home_team_id,
+    f.away_team_id,
+    c.name_en AS league,
+    f.kickoff_time,
+    ht.icon_url AS home_icon,
+    at.icon_url AS away_icon
+FROM fixtures f
+JOIN teams ht ON f.home_team_id = ht.id
+JOIN teams at ON f.away_team_id = at.id
+JOIN seasons s ON f.season_id = s.id
+JOIN competitions c ON s.competition_id = c.id
+WHERE f.season_id = 'PULSELIVE_SEASON_719'
+  AND f.kickoff_time > NOW()
+ORDER BY f.kickoff_time ASC
+LIMIT 10
     """)
     result = db.execute(query).fetchall()
     return {"upcomingGames" : [dict_to_camel_case(row._mapping) for row in result]}
