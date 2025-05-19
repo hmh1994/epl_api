c39a6d67ba8d1ff5bc727bc6f6a3c941802e1c8b
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-05-19 10:48:05 +0900

    Update database api

--- database_api.py
+++ database_api.py
@@ -2,54 +2,67 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
 from database import get_db
+from model import dict_to_camel_case
 
-router = APIRouter(prefix="/api/database", tags=["Data"])
+router = APIRouter(prefix="/api/database", tags=["Database"])
 
 @router.get("/competitions")
 def get_competitions(db: Session = Depends(get_db)):
     query = text("SELECT * FROM competitions")
     result = db.execute(query).fetchall()
-    return {"competitions": [dict(row._mapping) for row in result]}
+    return {"competitions": [dict_to_camel_case(row._mapping) for row in result]}
 
 @router.get("/fixtures")
 def get_fixtures(db: Session = Depends(get_db)):
     query = text("SELECT * FROM fixtures")
     result = db.execute(query).fetchall()
-    return {"fixtures": [dict(row._mapping) for row in result]}
+    return {"fixtures": [dict_to_camel_case(row._mapping) for row in result]}
 
 @router.get("/grounds")
 def get_grounds(db: Session = Depends(get_db)):
     query = text("SELECT * FROM grounds")
     result = db.execute(query).fetchall()
-    return {"grounds": [dict(row._mapping) for row in result]}
+    return {"grounds": [dict_to_camel_case(row._mapping) for row in result]}
 
-@router.get("/player_stats")
-def get_players(db: Session = Depends(get_db)):
+@router.get("/metadata")
+def get_metadata(db: Session = Depends(get_db)):
+    query = text("SELECT * FROM metadata")
+    result = db.execute(query).fetchall()
+    return {"metadata": [dict_to_camel_case(row._mapping) for row in result]}
+
+@router.get("/news")
+def get_news(db: Session = Depends(get_db)):
+    query = text("SELECT * FROM news")
+    result = db.execute(query).fetchall()
+    return {"news": [dict_to_camel_case(row._mapping) for row in result]}
+
+@router.get("/playerStats")
+def get_playerStats(db: Session = Depends(get_db)):
     query = text("SELECT * FROM player_stats")
     result = db.execute(query).fetchall()
-    return {"player_stats": [dict(row._mapping) for row in result]}
+    return {"playerStats": [dict_to_camel_case(row._mapping) for row in result]}
 
 @router.get("/players")
 def get_players(db: Session = Depends(get_db)):
     query = text("SELECT * FROM players")
     result = db.execute(query).fetchall()
-    return {"players": [dict(row._mapping) for row in result]}
+    return {"players": [dict_to_camel_case(row._mapping) for row in result]}
 
 @router.get("/seasons")
 def get_seasons(db: Session = Depends(get_db)):
     query = text("SELECT * FROM seasons")
     result = db.execute(query).fetchall()
-    return {"seasons": [dict(row._mapping) for row in result]}
+    return {"seasons": [dict_to_camel_case(row._mapping) for row in result]}
 
-@router.get("/team_stats")
-def get_standings(db: Session = Depends(get_db)):
+@router.get("/teamStats")
+def get_teamStats(db: Session = Depends(get_db)):
     query = text("SELECT * FROM team_stats")
     result = db.execute(query).fetchall()
-    return {"team_stats": [dict(row._mapping) for row in result]}
+    return {"teamStats": [dict_to_camel_case(row._mapping) for row in result]}
 
 
 @router.get("/teams")
 def get_teams(db: Session = Depends(get_db)):
     query = text("SELECT * FROM teams")
     result = db.execute(query).fetchall()
-    return {"teams": [dict(row._mapping) for row in result]}
+    return {"teams": [dict_to_camel_case(row._mapping) for row in result]}
