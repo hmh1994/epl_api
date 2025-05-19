5018dbee4ec8e6131827bf2af30e927432defb40
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-05-19 15:37:31 +0900

    Add team_rank (goal_for, against, points)

--- teams_api.py
+++ teams_api.py
@@ -27,3 +27,57 @@ def overall_teamrank(db : Session = Depends(get_db)):
     result = db.execute(query).fetchall()
     return {"overallTeamrank" : [dict_to_camel_case(row._mapping) for row in result]}
 
+@router.get("/top_score_team")
+def top_score_team(db : Session = Depends(get_db)):
+    query = text("""
+                SELECT
+                    RANK() OVER (ORDER BY ts.overall_goals_for DESC) AS rank,
+                    t.name_en AS team_name,
+                    c.name_en AS league_name,
+                    ts.overall_goals_for
+                FROM team_stats ts
+                JOIN teams t ON ts.team_id = t.id
+                JOIN seasons s ON ts.season_id = s.id
+                JOIN competitions c ON s.competition_id = c.id
+                ORDER BY ts.overall_goals_for DESC
+                LIMIT 5
+    """)
+    result = db.execute(query).fetchall()
+    return {"topScoreTeam" : [dict_to_camel_case(row._mapping) for row in result]}
+
+
+@router.get("/top_defend_team")
+def top_defend_team(db : Session = Depends(get_db)):
+    query = text("""
+                SELECT
+                    RANK() OVER (ORDER BY ts.overall_goals_against ASC) AS rank,
+                    t.name_en AS team_name,
+                    c.name_en AS league_name,
+                    ts.overall_goals_against
+                FROM team_stats ts
+                JOIN teams t ON ts.team_id = t.id
+                JOIN seasons s ON ts.season_id = s.id
+                JOIN competitions c ON s.competition_id = c.id
+                ORDER BY ts.overall_goals_against ASC
+                LIMIT 5
+    """)
+    result = db.execute(query).fetchall()
+    return {"topDefendTeam" : [dict_to_camel_case(row._mapping) for row in result]}
+
+@router.get("/top_points_team")
+def top_points_team(db : Session = Depends(get_db)):
+    query = text("""
+                SELECT
+                    RANK() OVER (ORDER BY ts.overall_points DESC) AS rank,
+                    t.name_en AS team_name,
+                    c.name_en AS league_name,
+                    ts.overall_points
+                FROM team_stats ts
+                JOIN teams t ON ts.team_id = t.id
+                JOIN seasons s ON ts.season_id = s.id
+                JOIN competitions c ON s.competition_id = c.id
+                ORDER BY ts.overall_points DESC
+                LIMIT 5
+    """)
+    result = db.execute(query).fetchall()
+    return {"topPointTeam" : [dict_to_camel_case(row._mapping) for row in result]}
