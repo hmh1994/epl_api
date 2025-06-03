063558557dc50fc679c3febb745c7e1a25080dbe
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-06-03 17:04:00 +0900

    .

--- news_api.py
+++ news_api.py
@@ -31,10 +31,12 @@ from sqlalchemy.orm import Session
 from database import get_db
 from models import News 
 from model import dict_to_camel_case
+from sqlalchemy.inspection import inspect
 
-router = APIRouter(prefix="/api/v1/news", tags=["News"])
+def orm_to_dict(obj):
+    return {c.key: getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs}
 
 @router.get("/list")
 def news_list(db: Session = Depends(get_db)):
     news_list = db.query(News).order_by(News.publish_date.desc()).all()
-    return {"newsList": [dict_to_camel_case(n.__dict__) for n in news_list]}
+    return {"newsList": [dict_to_camel_case(orm_to_dict(n)) for n in news_list]}
