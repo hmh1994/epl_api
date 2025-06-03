da22f47e6d9c70a9bdd419f1665c705da5ea3bf4
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-06-03 16:54:51 +0900

    .

--- news_api.py
+++ news_api.py
@@ -1,4 +1,4 @@
-'''from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text  
 from database import get_db
@@ -25,18 +25,3 @@ def newsList(db : Session = Depends(get_db)):
                  """)
     result = db.execute(query).fetchall()
     return {"newsList" : [dict_to_camel_case(row._mapping) for row in result]}
-'''
-
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-from database import get_db
-from models import News
-from schemas import NewsSchema
-from typing import Dict, List
-
-router = APIRouter(prefix="/api/v1/news", tags=["News"])
-
-@router.get("/list", response_model=Dict[str, List[NewsSchema]])
-def newsList(db: Session = Depends(get_db)):
-    news_list = db.query(News).order_by(News.publishDate.desc()).all()
-    return {"newsList": news_list}
