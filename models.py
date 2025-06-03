025c86e61921731ff8d1a88a83828ae8c3904d69
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-06-03 17:07:17 +0900

    .

--- models.py
+++ models.py
@@ -4,13 +4,13 @@ from database import Base
 class News(Base):
     __tablename__ = "news"
 
-    id = Column(Integer, primary_key=True, index=True)
-    title_en = Column(String)
-    title_kr = Column(String)
-    content_en = Column(String)
-    content_kr = Column(String)
-    newsLink = Column(String)
-    newsImg = Column(String)
-    source = Column(String)
-    type = Column(String)
-    publish_date = Column(DateTime)
+    id = Column("id", Integer, primary_key=True, index=True)
+    titleEnglish = Column("title_en", String)
+    titleKorean = Column("title_kr", String)
+    contentEnglish = Column("content_en", String)
+    contentKorean = Column("content_kr", String)
+    url = Column("url", String)
+    thumbnailUrl = Column("thumbnail_url", String)
+    source = Column("source", String)
+    type = Column("type", String)
+    publishDate = Column("publish_date", DateTime)
