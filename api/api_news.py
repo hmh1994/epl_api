95594b8bc4331ff9aff01fcd1ec32a7ebbd4b815
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-07-06 14:24:40 +0900

    2025-07-06

--- api/api_news.py
+++ api/api_news.py
@@ -10,23 +10,27 @@ router = APIRouter(prefix="/api/v1/news", tags=["News"])
 @router.get("/list")
 def news_list(db: Session = Depends(get_db)):
     query = text("""
-        select                  
-            publish_date as publishDate,
-            author_kr,
-            content_en,
-            content_kr,
-            url,
-            source,
-            teams,
-            thumbnail_url,
-            title_en,
-            title_kr,
-            type,
-            id,
-            author_en,
-            source_id
-        from news
-        order by publish_date DESC
+        SELECT
+            n.id AS news_id,
+            n.title_en,
+            n.title_kr,
+            n.content_en,
+            n.content_kr,
+            n.thumbnail_url AS news_img,
+            n.url AS news_url,
+            n.author_en,
+            n.author_kr,
+            STRING_AGG(t.name_en, ', ') AS team_name_en,
+            STRING_AGG(t.name_kr, ', ') AS team_name_kr,
+            n.type,
+            n.publish_date
+        FROM news_new n
+        LEFT JOIN news_team_association nta ON nta.news_id = n.id
+        LEFT JOIN teas_new t ON t.id = nta.team_id
+        GROUP BY 
+        n.id, n.title_en, n.title_kr, n.content_en, n.content_kr,
+        n.thumbnail_url, n.url, n.author_en, n.author_kr, n.type, n.publish_date
+        ORDER BY n.publish_date DESC
     """)
     
     result = db.execute(query).fetchall()
