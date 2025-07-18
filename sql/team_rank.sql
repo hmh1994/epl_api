8bccdf8d38d0c38fbf2c4d75411f4ac227c7eed2
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-07-18 12:18:55 +0900

    .

--- sql/team_rank.sql
+++ sql/team_rank.sql
@@ -10,14 +10,17 @@ SELECT
     RANK() OVER (
         ORDER BY ts.overall_points DESC, ts.overall_goals_difference DESC
     ) AS rank,
+    t.name_en,
+    t,name_kr,
     t.short_name_en,
+    t.short_name_kr,
     ts.team_id,
-    ts.overall_matches,
-    ts.overall_matches_won,
-    ts.overall_matches_drawn,
-    ts.overall_matches_lost,
-    ts.overall_goals_difference,
-    ts.overall_points,
+    ts.overall_matches AS matches,
+    ts.overall_matches_won AS won,
+    ts.overall_matches_drawn AS drawn,
+    ts.overall_matches_lost AS lost,
+    ts.overall_goals_difference AS gd,
+    ts.overall_points AS points,
     t.icon_url AS team_logo
 FROM team_stats_new ts
 JOIN teams_new t ON ts.team_id = t.id
