f3bcaa54dbd707d65b95e666d0e0e7b789681fa5
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-07-18 12:11:41 +0900

    .

--- api/api_team.py
+++ api/api_team.py
@@ -2,28 +2,14 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text 
 from lib.lib_database import get_db
+from lib.lib_sql import load_sql
 
 router = APIRouter(prefix="/api/v1/teams", tags=["Teams"])
 
 @router.get("/rank")
 def overall_teamrank(db : Session = Depends(get_db)):
-    query = text("""
-               SELECT 
-                    RANK() OVER (ORDER BY ts.overall_points DESC) AS rank,
-                    t.short_name_en,
-                    ts.team_id,
-                    ts.overall_matches,
-                    ts.overall_matches_won,
-                    ts.overall_matches_drawn,
-                    ts.overall_matches_lost,
-                    ts.overall_goals_difference,
-                    ts.overall_points,
-                    t.icon_url
-                FROM team_stats ts
-                JOIN teams t ON ts.team_id = t.id
-                WHERE ts.season_id = 'PULSELIVE_SEASON_719'
-                ORDER BY ts.overall_points DESC
-                 """)
+    sql = load_sql("team_rank.sql")
+    query = text(sql)   
     result = db.execute(query).fetchall()
     return {"overallTeamrank" : [dict(row._mapping) for row in result]}
 
---
+++ sql/team_rank.sql
@@ -0,0 +1,23 @@
+WITH latest_season AS (
+        SELECT s.id
+        FROM seasons_new s
+        JOIN competitions_new c ON s.competition_id = c.id
+        WHERE c.abbreviation = 'EN_PR'
+        ORDER BY s.date_end DESC
+        LIMIT 1
+    )
+               SELECT 
+                    RANK() OVER (ORDER BY ts.overall_points DESC) AS rank,
+                    t.short_name_en,
+                    ts.team_id,
+                    ts.overall_matches,
+                    ts.overall_matches_won,
+                    ts.overall_matches_drawn,
+                    ts.overall_matches_lost,
+                    ts.overall_goals_difference,
+                    ts.overall_points,
+                    t.icon_url AS team_logo
+                FROM team_stats_new ts
+                JOIN teams_new t ON ts.team_id = t.id
+                WHERE ts.season_id =  (SELECT id FROM latest_season)
+                ORDER BY ts.overall_points DESC
