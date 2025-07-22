d3addc028515adc81708b863448e7fcde681e91b
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-07-22 16:57:43 +0900

    2025-07-22

--- api/api_news.py
+++ api/api_news.py
@@ -7,11 +7,12 @@ from lib.lib_sql import load_sql
 
 router = APIRouter(prefix="/api/v1/news", tags=["News"])
 
-@router.get("/list")
-def news_list(db: Session = Depends(get_db)):
+@router.get("/list/{count}")
+def news_list(count: int, db: Session = Depends(get_db)):
     sql = load_sql("news_list.sql")
     query = text(sql)    
-    result = db.execute(query).fetchall()
+    result = db.execute(query, {"count": count}).fetchall()
     return {
         "newsList": [dict_to_camel_case(row._mapping) for row in result]
     }
+    
