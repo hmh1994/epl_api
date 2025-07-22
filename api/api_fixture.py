1aa243220711116c9da3facdc079d0b376758525
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-07-22 17:04:08 +0900

    .

--- api/api_fixture.py
+++ api/api_fixture.py
@@ -71,3 +71,60 @@ def match_up_by_date(timestamp: int, db: Session = Depends(get_db)):
         "end_utc": utc_end
     }).fetchall()
     return {"matchUpByDate": [dict_to_camel_case(row._mapping) for row in result]}
+
+@router.get("/range/{startdate}/{enddate}")
+def match_up_by_range(startdate: int, enddate: int, db: Session = Depends(get_db)):
+    kst = pytz.timezone("Asia/Seoul")
+
+    try:
+        dt_start_utc = datetime.utcfromtimestamp(startdate).replace(tzinfo=pytz.utc)
+        dt_end_utc = datetime.utcfromtimestamp(enddate).replace(tzinfo=pytz.utc)
+
+        dt_start_kst = dt_start_utc.astimezone(kst)
+        dt_end_kst = dt_end_utc.astimezone(kst)
+    except Exception:
+        return {"error": "Invalid timestamp."}
+
+    query = text("""
+    WITH latest_season AS (
+        SELECT s.id
+        FROM seasons_new s
+        JOIN competitions_new c ON s.competition_id = c.id
+        WHERE c.abbreviation = 'EN_PR'
+        ORDER BY s.date_end DESC
+        LIMIT 1
+    )
+    SELECT 
+        fx.id,
+        fx.kickoff_time,
+        ht.id as home_team_id, 
+        ht.name_en as home_team_en,
+        ht.name_kr as home_team_kr,
+        ht.short_name_en as short_home_team_en,
+        ht.short_name_kr as short_home_team_kr,
+        ht.icon_url as home_team_img,
+        fx.home_team_score,
+        at.id as away_team_id, 
+        at.name_en as away_team_en,
+        at.name_kr as away_team_kr,
+        at.short_name_en as short_away_team_en,
+        at.short_name_kr as short_away_team_kr,
+        at.icon_url as away_team_img,	
+        fx.away_team_score		
+    FROM fixtures_new fx
+    JOIN teams_new ht ON fx.home_team_id = ht.id
+    JOIN teams_new at ON fx.away_team_id = at.id
+    WHERE fx.season_id = (SELECT id FROM latest_season)
+    AND fx.kickoff_time >= :start_utc
+    AND fx.kickoff_time < :end_utc
+    ORDER BY fx.kickoff_time DESC
+    """)
+
+    result = db.execute(query, {
+        "start_utc": dt_start_utc,
+        "end_utc": dt_end_utc
+    }).fetchall()
+
+    return {
+        "matchUpByRange": [dict_to_camel_case(row._mapping) for row in result]
+    }
