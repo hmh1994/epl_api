aa104c27faf8a3c3bcc9e1e5cccc58d77e243d01
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-08-07 10:06:26 +0900

    .

--- api/api_fixture.py
+++ api/api_fixture.py
@@ -269,52 +269,57 @@ def match_up_by_range(startdate: int, enddate: int, db: Session = Depends(get_db
             grouped[date] = []
 
     return dict(grouped)
-
-@router.get("/infotest") #be582b72-34d8-4386-a222-f69d35d52546
+    
+@router.get("/infotest")
 def match_detail(db: Session = Depends(get_db)):
     query = text("""
     SELECT 
-    fx.id, 
-    g.name_en AS ground_name_en,
-    g.name_kr AS ground_name_kr,
-    g.city_name_en,
-    g.city_name_kr,
-    g.capacity,
-    fx.kickoff_time,
-    of.display_name_en AS official_name_en,
-    of.display_name_kr AS official_name_kr,
-    fx.home_team_id,
-    s1.display_name_en AS home_team_manager_en,
-    s1.display_name_kr AS home_team_manager_kr,
-    ma.home_team_formation,
-    fx.away_team_id,
-    s2.display_name_en AS away_team_manager_en,
-    s2.display_name_kr AS away_team_manager_kr,
-    ma.away_team_formation,
-    htl.home_lineup
-FROM fixtures_new fx
-JOIN matches_new ma ON fx.id = ma.fixture_id
-LEFT JOIN staffs_new s1 ON ma.home_team_manager = s1.id
-LEFT JOIN staffs_new s2 ON ma.away_team_manager = s2.id
-LEFT JOIN grounds_new g ON fx.ground_id = g.id
-LEFT JOIN officials_new of ON ma.official_main_referee_id = of.id
-LEFT JOIN LATERAL (
-    SELECT JSON_AGG(
-        JSON_BUILD_OBJECT(
-            'player_id', mht.player_id,
-            'shirt_number', mht.shirt_number,
-            'row', mht.row,
-            'column', mht.column
-        ) ORDER BY mht.shirt_number
-    ) AS home_lineup
-    FROM match_home_team_lineup_association mht
-    WHERE mht.match_id = ma.id
-) AS htl ON TRUE
-WHERE fx.id = '33e09323-9d46-45e1-a734-1b2bb968afb3';
-    """)    
+        fx.id, 
+        g.name_en AS ground_name_en,
+        g.name_kr AS ground_name_kr,
+        g.city_name_en,
+        g.city_name_kr,
+        g.capacity,
+        fx.kickoff_time,
+        of.display_name_en AS official_name_en,
+        of.display_name_kr AS official_name_kr,
+        fx.home_team_id,
+        s1.display_name_en AS home_team_manager_en,
+        s1.display_name_kr AS home_team_manager_kr,
+        ma.home_team_formation,
+        fx.away_team_id,
+        s2.display_name_en AS away_team_manager_en,
+        s2.display_name_kr AS away_team_manager_kr,
+        ma.away_team_formation,
+        htl.home_lineup
+    FROM fixtures_new fx
+    JOIN matches_new ma ON fx.id = ma.fixture_id
+    LEFT JOIN staffs_new s1 ON ma.home_team_manager = s1.id
+    LEFT JOIN staffs_new s2 ON ma.away_team_manager = s2.id
+    LEFT JOIN grounds_new g ON fx.ground_id = g.id
+    LEFT JOIN officials_new of ON ma.official_main_referee_id = of.id
+    LEFT JOIN LATERAL (
+        SELECT JSON_AGG(
+            JSON_BUILD_OBJECT(
+                'player_id', mht.player_id,
+                'shirt_number', mht.shirt_number,
+                'row', mht.row,
+                'column', mht.column
+            ) ORDER BY mht.shirt_number
+        ) AS home_lineup
+        FROM match_home_team_lineup_association mht
+        WHERE mht.match_id = ma.id
+    ) AS htl ON TRUE
+    WHERE fx.id = '33e09323-9d46-45e1-a734-1b2bb968afb3';
+    """)
+    
     result = db.execute(query).fetchone()
+
+    if not result:
+        return {"result": None}
+    
     return {
-        "result": dict_to_camel_case(result._mapping)
+        "result": dict_to_camel_case_obj(result._mapping)
     }
 
 
