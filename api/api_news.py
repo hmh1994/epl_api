d10d89a647e79619352c22c3c1b05150d953d9d3
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-09-12 10:48:16 +0900

    .

--- api/api_news.py
+++ api/api_news.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
 from lib.lib_database import get_db
-from schemas.news import NewsBase, NewsListResponse
+from schemas.schemas_news import NewsBase, NewsListResponse
 from typing import List
 
 router = APIRouter(prefix="/api/v1/news", tags=["News"])
