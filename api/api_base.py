930cec019bfdfcaa27305dc7646dc8b14e280b8b
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-09-17 14:31:49 +0900

    .

--- api/api_base.py
+++ api/api_base.py
@@ -5,9 +5,9 @@ from lib.lib_database import get_db
 
 router = APIRouter(prefix="/api/v1/dataVerify", tags=["Verify"])
 
-@router.get("/match_stats")
-def table_match_stats(db: Session = Depends(get_db)):
+@router.get("/match_stats", response_model=list[dict])
+def table_match_stats(db: Session = Depends(get_db)) -> list[dict]:
     sql = "SELECT * FROM match_stats LIMIT 1"
     query = db.execute(text(sql))
-    return {dict(row._mapping) for row in query.fetchall()}
-
+    results = [dict(row._mapping) for row in query.fetchall()]
+    return results
