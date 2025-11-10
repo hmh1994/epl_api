533b148f3a45f28cd69cb84ac723a2e42e047b17
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-11-10 15:05:36 +0900

    .

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 from routers import league_router, team_router
 
-#from api.api_database import router as database_router
+from api.api_database import router as database_router
 
 app = FastAPI(title="Football Data API", version="1.0",)
 
-#app.include_router(database_router)
+app.include_router(database_router)
 
 app.include_router(league_router.router)
 app.include_router(team_router.router)
