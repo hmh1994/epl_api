0a326636d479250edbd825b059ab8830cb5c0d69
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-11-14 17:28:03 +0900

    2025-11-14

--- routers/team_info_01.py
+++ routers/team_info_01.py
@@ -7,10 +7,10 @@ router = APIRouter(prefix="/api/v1", tags=["team_info_01.py"])
 
 @router.get("/teams")
 def get_teams(
-    leagueId: Optional[str] = None,
-    search: Optional[str] = None,
-    season: Optional[str] = None,
-    locale: Optional[str] = "ko-KR",
+    leagueId: Optional[str] = Query(None),
+    search: Optional[str] = Query(None),
+    season: Optional[str] = Query(None),
+    locale: Optional[str] = Query("ko-KR"),
     db: Session = Depends(get_db),
 ):
     sql = "SELECT * FROM teams"
