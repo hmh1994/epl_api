4146ca93c3e7f25ebf8b3f55b03f059ff141a3f1
Gyeongmin Kim <hmh.cs94@gmail.com>
2025-12-09 13:03:40 +0900

    2025-12-09

--- routers2/fetch_news_list.py
+++ routers2/fetch_news_list.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from typing import Optional
+from datetime import datetime
 from database import get_db
 
 router = APIRouter(prefix="/api/v1", tags=["fetch_news_list"])
